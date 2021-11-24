5e633b5dde85b11268a8cb5d681c360fabc1a88e
Klever Ferreira <kleverfp@gmail.com>
2021-11-24 16:39:19 -0300

    add control sensor component

--- client/src/components/dashboard/Sensor.js
+++ client/src/components/dashboard/Sensor.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {Link,useParams,useNavigate } from 'react-router-dom';
@@ -10,13 +10,25 @@ import Spinner from '../layout/Spinner';
 const Sensor = ({getSensors,sendMessageToServer,socketConnection,deleteSensor,sensor:{sensor}}) => {
     const navigate = useNavigate();
     const {gatewayid} = useParams();
-    
+    let showStatus = false;
     useEffect(()=>{
         getSensors(gatewayid);
         socketConnection(gatewayid);
         
     },[]);
-   
+
+    const [show,setShow] = useState([]);
+    const showControlsHandler = (id)=>{
+
+        const index = show.findIndex((sw)=>sw.id==id);
+    
+        if(index > -1)
+            setShow(show.filter((ob)=>ob.id !== id));
+        else
+            setShow([...show,{id}]);
+    }
+        
+
     const createSensorHandler = (e)=>{
         navigate(`/create-sensor/${gatewayid}`);
     }    
@@ -27,12 +39,14 @@ const Sensor = ({getSensors,sendMessageToServer,socketConnection,deleteSensor,se
 
     const serverMessageHandler = (sensorid)=>{
        sendMessageToServer(sensorid,gatewayid);
+       
     }
       
     const deleteHandler= async (sensorid) =>{
             deleteSensor(sensorid,gatewayid);
     }
 
+   
     return (
         <section className="container">{
             (sensor ===null)  ? (<Spinner/>):(
@@ -51,18 +65,34 @@ const Sensor = ({getSensors,sendMessageToServer,socketConnection,deleteSensor,se
                                 </tr>
                             </thead>
                             <tbody>{sensor.map(snr =>(
+                                <Fragment key={`frg-01${snr.sensorid}`}>
                                 <tr key={snr.sensorid}>
                                     <td>{snr.sensorid}</td>
                                     <td className={`hide-sm msg-${snr.status}`}>{snr.name}</td>
                                     <td className={`hide-sm msg-${snr.status}`}>{snr.status}</td>
                                     <td className={`hide-sm msg-${snr.status}`}>{snr.created_at}</td>
                                     <td>
-                                    <button onClick={ () =>serverMessageHandler(snr.sensorid)} className="btn btn-success">Send</button>
+                                        <button onClick={ () =>showControlsHandler(snr.sensorid)} className="btn btn-success">Commands</button>
                                     </td>
                                     <td>
-                                    <button onClick={ () =>deleteHandler(snr.sensorid)} className="btn btn-danger">Delete</button>
+                                        <button onClick={ () =>deleteHandler(snr.sensorid)} className="btn btn-danger">Delete</button>
                                     </td>
-                                </tr>))}
+                                </tr>
+                                {(show.findIndex((obj)=>obj.id==snr.sensorid) > -1) && 
+                                    
+                                    <tr>
+                                        <td><input type="text" placeholder="type"></input></td>
+                                        <td><input type="text" placeholder="value"></input></td>
+                                        <td><button onClick={ () =>showControlsHandler(snr.sensorid)} className="btn btn-success">send</button></td>
+                                        <td><button onClick={ () =>showControlsHandler(snr.sensorid)} className="btn btn-success">reset</button></td>
+                                    </tr>
+                                    } 
+                                </Fragment>
+                                
+                        
+                            ))}
+                               
+
                             </tbody>
                         </table>
                         <button onClick={createSensorHandler} className="btn btn-primary my-1">Create sensor</button>
