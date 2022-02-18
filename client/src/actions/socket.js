9214c7aca9f1bd7140dc721b2070ab31262385fe
Klever Ferreira <kleverfp@gmail.com>
2022-02-18 16:49:42 -0300

    init wifi gateway

--- client/src/actions/socket.js
+++ client/src/actions/socket.js
@@ -12,7 +12,7 @@ let socket=[];
 export const socketConnection = (gatewayid)=>dispatch=>{
 
 
-    socket = io("http://127.0.0.1:5005",{
+    socket = io("https://qonteciot.com/socket.io/",{
         auth:{
             token:localStorage.token
         },
